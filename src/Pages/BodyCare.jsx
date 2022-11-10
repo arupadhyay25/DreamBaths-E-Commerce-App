e404d5cfecf02902653a02645a426122cb5711b7
arupadhyay25 <arupadhyay25@gmail.com>
2022-11-10 09:09:17 +0530

    db.json

--- src/Pages/BodyCare.jsx
+++ src/Pages/BodyCare.jsx
@@ -5,17 +5,24 @@ import axios from "axios";
 
 export const BodyCare = () => {
   let [state, setstate] = useState([]);
+  let [order, setorder] = useState("");
+  let [page, setpage] = useState(1);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products").then((r)=>setstate(r.data))
-  }, []);
+    axios.get(`http://localhost:8080/products`).then((r) => setstate(r.data));
+  }, [page, order]);
 
   return (
     <div>
       Body_care
+      <br />
+      <button onClick={() => setpage(1)}>1</button>
+      <button onClick={() => setpage(2)}>2</button>
+      <button onClick={() => setpage(3)}>3</button>
+      <br />
       <div className="bodycare-products">
         {state
-          .filter((e) => Number(e.price))
+          .filter((e) => e.category === "mensshop")
           .map((e, i) => (
             <Singleproduct
               key={i}
