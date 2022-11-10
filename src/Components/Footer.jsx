c64043084de4f51fa2db7ff80ba42c8b7e174d61
arupadhyay25 <arupadhyay25@gmail.com>
2022-11-10 10:07:37 +0530

    up to date

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import { FaFacebook, FaPinterest, FaInfo } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { IoLogoInstagram } from "react-icons/io";
+import { FaTiktok } from "react-icons/fa";
+import { TfiYoutube } from "react-icons/tfi";
+export const Footer = () => {
+  const column_1 = [
+    "Help & FAQs",
+    "Shipping",
+    "Returns & Exchanges",
+    "Order Tracking",
+    "Corporate Sales & Gifts",
+    "Contact Us",
+  ];
+  const column_2 = [
+    "Sign In or Sign Up",
+    "Order Tracking",
+    "My Auto Refresh",
+    "My Love-It List",
+  ];
+  const column_3 = [
+    "About Us",
+    "Careers",
+    "Gift Cards",
+    "Shop by Fragrance",
+    "Product Ingredients",
+    "Get Inspired",
+    "Diversity, Equity & Inclusion",
+  ];
+  const column_4 = ["Store Locator", "Global Locations"];
+  const bhai = [
+    "Terms Of Use",
+    "Privacy Policy",
+    "Security Bug Report",
+    "States Privacy Rights Notice (Certain States)",
+    "Do Not Sell or Share My Personal Information (Certain States)",
+    "Transparency in Supply Chains",
+  ];
+  const bhai2 = [
+    "Targeted Ad Preferences (Certain States)",
+    " Limit the Use of My Sensitive Personal Information (California)",
+  ];
+  const FooterColumn = (props) => {
+    const { head, arr } = props;
+    return (
+      <div className={styles.Footer_Two_column}>
+        <div className={styles.Footer_Two_columnHead}>{head}</div>
+        {arr.map((el) => (
+          <div className={styles.Footer_Two_columnChild}>{el}</div>
+        ))}
+      </div>
+    );
+  };
+  return (
+    <div className={styles.Footer}>
+      <hr />
+      <div className={styles.Footer_Main}>
+        <div className={styles.Footer_One}>
+          <div className={styles.Footer_One_Head}>
+            GET EMAIL OFFERS & THE LATEST NEWS FROM BATH & BODY WORKS!
+          </div>
+          <div>
+            <div className={styles.Footer_One_Child}>Enter Email</div>
+            <div className={styles.Footer_One_Child_box}>
+              <div>
+                <input type="text" />
+              </div>
+              <div>
+                <FaInfo className={styles.Footer_info} />
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className={styles.Footer_One_Child}>Confirm Email</div>
+            <div className={styles.Footer_One_Child_box}>
+              <div>
+                <input type="text" />
+              </div>
+            </div>
+            <button className={styles.Footer_One_Child_submit}>SUBMIT</button>
+          </div>
+          <div>
+            <div className={styles.Footer_One_Icons_Main}>Get Connected</div>
+            <div className={styles.Footer_One_Icons}>
+              <FaFacebook className={styles.Facebook} />
+              <AiFillTwitterCircle className={styles.Twitter} />
+              <IoLogoInstagram className={styles.Instagram} />
+              <FaTiktok className={styles.TikTok} />
+              <TfiYoutube className={styles.Youtube} />
+              <FaPinterest className={styles.pin} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.Footer_Two}>
+          <FooterColumn head="CUSTOMER CARE" arr={column_1} />
+          <FooterColumn head="MY ACCOUNT" arr={column_2} />
+          <FooterColumn head="DISCOVER" arr={column_3} />
+          <FooterColumn head="FIND US" arr={column_4} />
+        </div>
+      </div>
+      <div className={styles.Footer_Main2}>
+        <div>
+          <div className={styles.Footer_Main21}>
+            {bhai.map((el) => (
+              <div className={styles.Footer_Main2_child}>{el}</div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className={styles.Footer_Main22}>
+            {bhai2.map((el) => (
+              <div className={styles.Footer_Main2_child}>{el}</div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.Footer_Main3}>
+          © 2022 Bath & Body Works Direct, Inc. All Rights Reserved
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,153 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "./logo.png";
+import Slider from "react-slick";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [isMobail, setisMobail] = useState(false);
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+  };
+
   return (
-    <div>
-      <div className="navbar-links">
-        <Link to="/">Navbar</Link>
-        <Link to="/login">Login/Signup</Link>
+    <>
+      <Slider {...settings} className="slid">
+        <div>
+          <h3>10% off for new users on first order</h3>
+        </div>
+        <div>
+          <h3>FREE SHIPPING on PREPAID Orders</h3>
+        </div>
+        <div>
+          <h3>USE CODE : MASAI10 TO GET 10% OFF</h3>
+        </div>
+      </Slider>
+
+      <div className="navbar">
+        <div className="logosection">
+          <div>
+            <button className="drawer" onClick={() => setisMobail(!isMobail)}>
+              <i class="fa-solid fa-bars"></i>
+            </button>
+          </div>
+          <div>
+            <img src={logo} alt="" className="logo" />
+          </div>
+
+          <div className="signupsection">
+            <button className="logbtn">
+              <Link to="/login">
+                <i class="fa-regular fa-user"></i>
+              </Link>
+            </button>
+            <button className="logbtn">
+              <i class="fa-sharp fa-solid fa-heart"></i>
+            </button>
+            <button className="cartbtn-0nav">
+              <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+        <div className="navbar-links">
+          <Link className="pagelinks-cat012" to="/top-offers">
+            Top Offers
+          </Link>
+          <Link className="pagelinks-cat012" to="/body-care">
+            Body Care
+          </Link>
+          <Link className="pagelinks-cat012" to="/candles">
+            Candles
+          </Link>
+          <Link className="pagelinks-cat012" to="/home-fragrance">
+            Wallflower & AirFreshners
+          </Link>
+          <Link className="pagelinks-cat012" to="/hand-soaps-sanitizers">
+            Hand Soaps & Sanitizers{" "}
+          </Link>
+          <Link className="pagelinks-cat012" to="/mens-shop">
+            Men's Shop
+          </Link>
+          <Link className="pagelinks-cat012" to="/moxy-collection">
+            Moxy
+          </Link>
+          <Link className="pagelinks-cat012" to="holiday-shop">
+            Holiday Shop
+          </Link>
+        </div>
       </div>
-      <br />
-      <div className="navbar-links">
-        <Link to="/top-offers">Top Offers</Link>
-        <Link to="/body-care">Body Care</Link>
-        <Link to="/candles">Candles</Link>
-        <Link to="/home-fragrance">Wallflower & AirFreshners</Link>
-        <Link to="/hand-soaps-sanitizers">Hand Soaps & Sanitizers </Link>
-        <Link to="/mens-shop">Men's Shop</Link>
-        <Link to="/moxy-collection">Moxy</Link>
-        <Link to="holiday-shop">Holiday Shop</Link>
+      <div className={isMobail ? "resdrawer" : "norespon"}>
+        <div className="closebuton">
+          <button
+            onClick={() => setisMobail(!isMobail)}
+            className="btnclosedrawer-987"
+          >
+            {" "}
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+        <div className="navbar-links-drawer">
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            {" "}
+            <Link className="drawer-links-0934" to="/top-offers">
+              Top Offers
+            </Link>
+          </h4>
+
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934" to="/body-care">
+              Body Care
+            </Link>
+          </h4>
+
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934" to="/candles">
+              Candles
+            </Link>
+          </h4>
+
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934" to="/home-fragrance">
+              Wallflower & AirFreshners
+            </Link>
+          </h4>
+
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934" to="/hand-soaps-sanitizers">
+              Hand Soaps & Sanitizers{" "}
+            </Link>
+          </h4>
+
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934" to="/mens-shop">
+              Men's Shop
+            </Link>
+          </h4>
+
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934" to="/moxy-collection">
+              Moxy
+            </Link>
+          </h4>
+
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934" to="holiday-shop">
+              Holiday Shop
+            </Link>
+          </h4>
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934">WishList</Link>
+          </h4>
+          <h4 onClick={() => setisMobail(!isMobail)}>
+            <Link className="drawer-links-0934">Login</Link>
+          </h4>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
